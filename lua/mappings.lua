38ce1909e65f3987a413ca481429c8010bb52456
diamond2sword <diamond2sword@gmail.com>
2025-02-28 13:29:34 +0800

    update project

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -21,36 +21,36 @@ map("v","<C-C>",
   end
 )
 
-local _gitbashpath = function()
-  local buf_path = vim.api.nvim_buf_get_name(0)
-  local buf_dir = vim.fn.fnamemodify(buf_path, ":h")
-  local repo_dir = vim.fn.systemlist("git -C "..buf_dir.." rev-parse --show-toplevel")[1]
-  -- return repo_dir.."/git.bash"
-  local git_bash_path = repo_dir.."/git.bash"
-  if not vim.loop.fs_stat(git_bash_path) then
-    vim.notify("no git.bash in repo", vim.log.levels.WARN)
-    return nil
-  end
-  return git_bash_path
-end
+-- local _gitbashpath = function()
+--   local buf_path = vim.api.nvim_buf_get_name(0)
+--   local buf_dir = vim.fn.fnamemodify(buf_path, ":h")
+--   local repo_dir = vim.fn.systemlist("git -C "..buf_dir.." rev-parse --show-toplevel")[1]
+--   -- return repo_dir.."/git.bash"
+--   local git_bash_path = repo_dir.."/git.bash"
+--   if not vim.loop.fs_stat(git_bash_path) then
+--     vim.notify("no git.bash in repo", vim.log.levels.WARN)
+--     return nil
+--   end
+--   return git_bash_path
+-- end
+--
+-- local _git = function(git_args)
+--   local gitbashpath = _gitbashpath()
+--   if not gitbashpath then return end
+--   local cmd = 'bash '.._gitbashpath()..' '..git_args
+--   vim.fn.jobstart(cmd, {
+--     on_stdout = function()
+--       vim.notify_once(git_args)
+--     end
+--   })
+-- end
 
-local _git = function(git_args)
-  local gitbashpath = _gitbashpath()
-  if not gitbashpath then return end
-  local cmd = 'bash '.._gitbashpath()..' '..git_args
-  vim.fn.jobstart(cmd, {
-    on_stdout = function()
-      vim.notify_once(git_args)
-    end
-  })
-end
 
+-- map("n", "<Leader>g<C-S>", function() _git("push") end,
+--   { desc = "Push" })
+local _git = require('fn.git.cmd')
 map("n", "<Leader>g", "", { desc = "Git" })
-
-map("n", "<Leader>g<C-S>", function() _git("push") end,
-  { desc = "Push" })
-map("n", "<Leader>g<C-S>", require('fn.git.push'),
-  { desc = "Push" })
+map("n", "<Leader>g<C-S>", function() _git('push') end, { desc = "Push" })
 
 map("n", "<Leader>g<C-L>", function() _git("login") end,
   { desc = "Login" })
